c77d3408ab438bdfafae83094f52cb89fc436afb
TwelveNight <1315640046@qq.com>
2024-04-04 23:10:51 +0800

    hyprland:update swwwreload.sh

--- private_dot_config/hypr/scripts/executable_swwwreload.sh
+++ private_dot_config/hypr/scripts/executable_swwwreload.sh
@@ -1,10 +1,8 @@
 swww query
 if [[ $? -eq 1 ]]; then
 	ls /run/user/1000/swww.socket
-	if [[ $? -eq 1 ]]; then
-		echo "Socket removed"
-	else
+	if [[ $? -ne 1 ]]; then
 		rm /run/user/1000/swww.socket
 	fi
-	swww-daemon &
+	swww-daemon --format xrgb
 fi
