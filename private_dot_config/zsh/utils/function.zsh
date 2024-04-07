5cc98a726b9dabc1e52b60e7a10c160377e4a8dc
TwelveNight <1315640046@qq.com>
2024-04-07 22:12:29 +0800

    zsh:yazi wrapper

---
+++ private_dot_config/zsh/utils/function.zsh
@@ -0,0 +1,11 @@
+# using yy shell wrapper that provides the ability to change the current working directory when exiting Yazi.
+function yy() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+	yazi "$@" --cwd-file="$tmp"
+	if cwd="$(/usr/bin/cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+		cd "$cwd"
+	fi
+	rm -f -- "$tmp"
+}
+
+alias aa='yy'
