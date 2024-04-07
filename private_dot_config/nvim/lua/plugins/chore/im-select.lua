9a90860ef2c46b582584f1cdd7fe7e1636d64eb2
TwelveNight <1315640046@qq.com>
2024-04-07 22:11:23 +0800

    nvim:do not change back to the zh

--- private_dot_config/nvim/lua/plugins/chore/im-select.lua
+++ private_dot_config/nvim/lua/plugins/chore/im-select.lua
@@ -2,7 +2,10 @@ return {
   {
     "keaising/im-select.nvim",
     config = function()
-      require("im_select").setup({})
+      require("im_select").setup({
+        -- not auto change back to zh after re-enter insert mode
+        set_previous_events = {},
+      })
     end,
   },
 }
