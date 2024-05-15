df633cef23d1b0dc89b8965b35889b36336bb609
TwelveNight <1315640046@qq.com>
2024-05-15 16:34:44 +0800

    hyprland:sync and update

--- private_dot_config/hypr/scripts/executable_moveworkspace.sh
+++ private_dot_config/hypr/scripts/executable_moveworkspace.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-hyprctl workspaces | rg workspace | rg eDP-1 | awk '{print $3}' | while read workspace_id; do
+hyprctl workspaces | rg workspace | rg eDP-2 | awk '{print $3}' | while read workspace_id; do
 	if [ "$workspace_id" -lt 11 ]; then
-		monitor_id=$(hyprctl workspaces | grep workspace | grep -v eDP-1 | awk '{print $7}' | cut -d ':' -f1 | xargs echo)
+		monitor_id=$(hyprctl workspaces | grep workspace | grep -v eDP-1 | grep -v eDP-2 | awk '{print $7}' | head -1 | sed 's/://g')
 		hyprctl dispatch moveworkspacetomonitor $workspace_id $monitor_id
 	fi
 done
